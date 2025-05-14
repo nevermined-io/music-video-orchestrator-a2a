0615ae51cd554928753490b8ad5df8a205bff134
Charly López <clriesco@gmail.com>
2025-05-14 12:47:09 +0200

    removed old behaviour

--- src_old/logger/logger.ts
+++
@@ -1,9 +0,0 @@
-import pino from "pino";
-import pretty from "pino-pretty";
-
-/**
- * Provides a configured logger using the pino library.
- * Transport is set to "pino-pretty" for human-readable output.
- * Default log level is "info".
- */
-export const logger = pino(pretty({ sync: true }));
